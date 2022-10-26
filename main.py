cb4d47941507d42015bc6222e3e2ebbc3cc4cd7d
Koji1227 <115434297+Koji1227@users.noreply.github.com>
2022-10-26 14:28:44 +0900

    test for mathematical question

--- main.py
+++ main.py
@@ -3,15 +3,16 @@
 # Shift+F10 を押して実行するか、ご自身のコードに置き換えてください。
 # Shift を2回押す を押すと、クラス/ファイル/ツールウィンドウ/アクション/設定を検索します。
 
-
-def print_hi(name):
+import math
+def print_sum(a, b):
+    c = math.sin(a + b)
     # スクリプトをデバッグするには以下のコード行でブレークポイントを使用してください。
-    print(f'Hi, {name}')  # Ctrl+F8を押すとブレークポイントを切り替えます。
-
+    print(f'{a}と{b}を足してsin({a+b})をとると{c}になるらしい。')  # Ctrl+F8を押すとブレークポイントを切り替えます。
 
 # ガター内の緑色のボタンを押すとスクリプトを実行します。
-if __name__ == '__main__':
-    name = input("あなたのお名前は? ")
-    print_hi(name)
+if __name__ == '__main__': # なくてもよい
+    x = int(input("xの値? "))
+    y = int(input("yの値? "))
+    print_sum(x, y)
 
 # PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください
